47025969335341c1bcec44bf4dabaf32a577f903
Meeri <meeri.simpanen@helsinki.fi>
2022-04-14 09:32:03 +0300

    testejä

--- budget-app/src/tests/budget_repository_test.py
+++ budget-app/src/tests/budget_repository_test.py
@@ -14,3 +14,18 @@ class TestBudget(unittest.TestCase):
         self.budget.deposit(1000, "Salary")
         self.budget.withdrawal(100, "gas")
         self.assertEqual(self.budget.balance, 900)
+
+    def test_ledger_works(self):
+        self.budget.deposit(100, "salary")
+        self.budget.check_ledger()
+        self.assertEqual(self.budget.ledger, [{'amount': 100, 'category': 'salary'}])
+
+    def test_get_balance_works(self):
+        self.budget.deposit(100, "salary")
+        self.budget.get_balance()
+        self.assertEqual(self.budget.balance, 100)
+
+    def test_withdrawal_doesnt_work_if_negative(self):
+        self.budget.deposit(100, "salary")
+        self.budget.withdrawal(200, "food")
+        self.assertEqual(self.budget.balance, 100)
