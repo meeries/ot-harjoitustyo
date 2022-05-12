08ce6b346bf22188d5b59bcb883e6d5de37c6062
Meeri <meeri.simpanen@helsinki.fi>
2022-05-12 22:16:42 +0300

    fixed

--- budget-app/src/build.py
+++ budget-app/src/build.py
@@ -1,8 +1,9 @@
 from db.initialize_database import initialize_database, drop_tables
+from db.database_connection import connection
 
 
 def build():
-    drop_tables()
+    drop_tables(connection)
     initialize_database()
 
 if __name__ == '__main__':
